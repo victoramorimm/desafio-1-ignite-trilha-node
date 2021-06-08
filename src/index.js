bbcbaedd831a167ef23ed6f477b435ff54d1829b
Victor Amorim <victorvmrgamer@gmail.com>
2021-06-08 08:15:45 -0300

    feat: create-users route

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { username, name } = request.body
+
+  const newUser = {
+    id: uuidv4(), 
+    username,
+    name,
+    todos: []
+  }
+
+  users.push(newUser)
+
+  return response.status(201).json(newUser)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
