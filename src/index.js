bee8a8cb5969abf830b38b5c590d9efcf0bbfa5a
Victor Amorim <victorvmrgamer@gmail.com>
2021-06-12 22:10:46 -0300

    feat: create list and generate todo

--- src/index.js
+++ src/index.js
@@ -11,7 +11,21 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  if (!username) {
+    return response.status(400).json({ error: 'Username not provided!' });
+  }
+
+  const userFoundByUsername = users.find((user) => user.username === username)
+
+  if (!userFoundByUsername) {
+    return response.status(400).json({ error: 'User not found by username!' });
+  }
+
+  request.user = userFoundByUsername;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
@@ -30,11 +44,28 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+
+  const todos = user.todos;
+
+  return response.status(200).send(todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { title, deadline } = request.body
+
+  const newTodo = {
+    id: uuidv4(),
+    done: false,
+    title,
+    created_at: new Date(),
+    deadline: new Date(deadline)
+  }
+
+  user.todos.push(newTodo)
+
+  return response.status(201).json(newTodo)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
